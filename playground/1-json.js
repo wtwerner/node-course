5efec1bc72992afbdad0cf882a423a790393c749
wtwerner <wtwerner@gmail.com>
2021-03-25 14:04:26 -0500

    Play with fs and JSON

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,15 +1,11 @@
 const fs = require('fs')
 
-// const book = {
-//     title: 'Ego is the Enemy',
-//     author: 'Ryan Holiday'
-// }
-
-// const bookJSON = JSON.stringify(book)
-// fs.writeFileSync('1-json.json', bookJSON)
-
 const dataBuffer = fs.readFileSync('1-json.json')
 const dataJSON = dataBuffer.toString()
 const data = JSON.parse(dataJSON)
 
-console.log(data.title)
+data.name = "Tommy"
+data.age = 33
+
+const newJSON = JSON.stringify(data)
+fs.writeFileSync('1-json.json', newJSON)
