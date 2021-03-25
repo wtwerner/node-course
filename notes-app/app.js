a04916c02b970abc719e839d3feacaa95240135f
wtwerner <wtwerner@gmail.com>
2021-03-25 13:44:25 -0500

    Install and configure yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,57 @@
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 const chalk = require('chalk')
 
-const command = process.argv[2]
+// Customize yargs verion
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note')
-} else if (command === 'remove') {
-    console.log('Removing note')
-}
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function () {
+        console.log('Listing all notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read the note',
+    handler: function () {
+        console.log('Reading the note')
+    }
+})
+
+yargs.parse()
