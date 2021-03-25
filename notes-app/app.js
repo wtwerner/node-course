4ce6ee398b9db4db787672a909310599aa4589db
wtwerner <wtwerner@gmail.com>
2021-03-25 13:16:32 -0500

    Take command line arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,10 @@
 const getNotes = require('./notes.js')
 const chalk = require('chalk')
 
-const msg = getNotes()
-console.log(msg)
+const command = process.argv[2]
 
-console.log(chalk.green('Success!'))
+if (command === 'add') {
+    console.log('Adding note')
+} else if (command === 'remove') {
+    console.log('Removing note')
+}
