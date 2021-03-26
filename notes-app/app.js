828628ef5f241da82e037363bd0e0e777601ddeb
wtwerner <wtwerner@gmail.com>
2021-03-25 20:07:21 -0500

    Refactor to arrow functions

--- notes-app/app.js
+++ notes-app/app.js
@@ -21,7 +21,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -37,7 +37,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.removeNote(argv.title)
     }
 })
@@ -46,7 +46,7 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'List all notes',
-    handler: function () {
+    handler() {
         console.log('Listing all notes')
     }
 })
@@ -55,7 +55,7 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read the note',
-    handler: function () {
+    handler() {
         console.log('Reading the note')
     }
 })
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -9,20 +9,16 @@ const addNote = (title, body) => {
     const notes = loadNotes()
     const duplicateNotes = notes.filter(note => note.title === title)
 
-
     if (duplicateNotes.length === 0) {
         notes.push({
             title: title,
             body: body
         })
-    
         saveNotes(notes)
-
         console.log(chalk.green.inverse('New note added'))
     } else {
         console.log(chalk.red.inverse('Note title taken'))
     }
-
 }
 
 const removeNote = (title) => {
