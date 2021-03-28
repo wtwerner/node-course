84fbf82d4a440f679e4b15e070031648ee6fc144
wtwerner <wtwerner@gmail.com>
2021-03-27 19:22:27 -0500

    Refactor using destructuring

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,17 +6,17 @@ const location = process.argv[2]
 if (!location) {
     console.log('Please provide a location')
 } else {
-    geocode(location, (err, data) => {
+    geocode(location, (err, { latitude, longitude, location } = {}) => {
         if (err) {
             return console.log(err)
         } 
     
-        forecast(data.latitude, data.longitude, (err, forecastData) => {
+        forecast(latitude, longitude, (err, forecastData) => {
             if (err) {
                 return console.log(err)
             } 
     
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
         })
     })
