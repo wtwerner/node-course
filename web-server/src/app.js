b4da7de50d5caf47a2b65fc73dfe6c237d6778b0
wtwerner <wtwerner@gmail.com>
2021-03-29 12:31:40 -0500

    Render dynamic content with handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Tommy Werner'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Tommy Werner'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This is the help message.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
