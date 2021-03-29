a43c02d9a39e1482455bd0ee18f9254d9292dde2
wtwerner <wtwerner@gmail.com>
2021-03-29 11:44:15 -0500

    Serve HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,25 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send({
+        name: 'Tommy',
+        age: 33
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        forecast: 'Sunny',
+        location: 'Winnetka'
+    })
 })
 
 app.listen(3000, () => {
