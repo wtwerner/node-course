39f134a828571c4a24828275330b4ef870c3749a
wtwerner <wtwerner@gmail.com>
2021-03-31 16:20:34 -0500

    Create form to input location

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,21 @@
 console.log('Client side javascript file is loaded')
 
-// fetch('http://puzzle.mead.io/puzzle').then(response => {
-//     response.json().then(data => {
-//         console.log(data)
-//     })
-// })
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch('http://localhost:3000/weather?address=chicago').then(response => {
-    response.json().then(data => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const location = search.value
+    
+    fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
